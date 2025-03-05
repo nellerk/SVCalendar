5e7f2e1aa50c5e234513a93b7346abb705eced76
Kristóf Neller <neller.kristof@outlook.com>
2025-03-05 12:36:36 +0100

    googleSync

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,1 +1,11 @@
-// place files you want to import through the `$lib` alias in this folder.
+import Ical from 'node-ical';
+const data = await Ical.async.fromULR(
+    'https://calendar.google.com/calendar/ical/d2a4e81eb4f8e4665911a343e79113f1f25c476c23fd1cb7804c4fe28017babd%40group.calendar.google.com/public/basic.ics'
+);
+
+export const events = {events: Object.values(data).filter(v => v.type === 'VEVENT').map(v => ({
+    start: new Date(v.start), 
+    end: new Date(v.end), 
+    text: v.summary, 
+    location: v.location
+}))}
